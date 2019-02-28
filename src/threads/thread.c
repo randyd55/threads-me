deace851f0e3abd09fafd7b7a6231d136025dc3a
Anthony Moeller <anthonym@cs.utexas.edu>
2019-02-28 17:00:35 -0600

    Added proper priority setting

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -351,7 +351,8 @@ thread_foreach (thread_action_func *func, void *aux)
 void
 thread_set_priority (int new_priority) 
 {
-  thread_current ()->priority = new_priority;
+  if(list_empty(&thread_current()->donation_list))
+    thread_current ()->priority = new_priority;
   thread_current () ->old_priority = new_priority;
   if(new_priority < list_entry(list_begin(&ready_list),struct thread, elem)->priority){
     thread_yield();
